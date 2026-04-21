40fc2e48ecfa9fea9ef25ce946626e8d0d18e686
ShepherdTausch <238777388+ShepherdTausch@users.noreply.github.com>
2026-04-21 15:07:52 -0400

    Entries are sorted, dir have top priority above files.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 import os, sys
 from datetime import datetime
 
+def sort_helper(entry):
+    entry_lower = entry.name.lower()
+    if entry.is_dir():
+        return (0, entry_lower) # is a dir, higher priority
+    else:
+        return (1, entry_lower) # not a dir, lower priority
+
 def lsdir():
-    output = sorted(os.scandir("."), key=lambda f: f.name.lower())
-    for file in output:
-        stat = file.stat()
-        if file.is_dir():
-            print(f"dir: {file.name}")
+    output = sorted(os.scandir("."), key=sort_helper)
+    for entry in output:
+        stat = entry.stat()
+        if entry.is_dir():
+            print(f"dir: {entry.name}")
         else:
-            print(f"file: {file.name}")
+            print(f"entry: {entry.name}")
 
 def main():
     lsdir()
