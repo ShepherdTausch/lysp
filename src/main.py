2f73023c83d1d94c1b6949f1c8b46e44a22d3072
ShepherdTausch <238777388+ShepherdTausch@users.noreply.github.com>
2026-04-21 12:10:59 -0400

    files are listed in sorted order

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
 import os, sys
+from datetime import datetime
 
-print(os.getcwd())
+def lsdir():
+    output = sorted(os.scandir("."), key=lambda f: f.name.lower())
+    for file in output:
+        stat = file.stat()
+        if file.is_dir():
+            print(f"dir: {file.name}")
+        else:
+            print(f"file: {file.name}")
+
+def main():
+    lsdir()
+
+if __name__ == "__main__":
+    main()
