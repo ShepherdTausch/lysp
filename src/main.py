6092da0540d67986ea70c7146ea15ef152ef75ed
ShepherdTausch <238777388+ShepherdTausch@users.noreply.github.com>
2026-04-23 17:13:42 -0400

    Add basic color support, removed goofy 'dir:' and 'entry:' prefixes

--- src/main.py
+++ src/main.py
@@ -1,21 +1,23 @@
 import os, sys
+import pwd
 from datetime import datetime
+from termcolor import colored
 
 def sort_helper(entry):
     entry_lower = entry.name.lower()
     if entry.is_dir():
-        return (0, entry_lower) # is a dir, higher priority
+        return (0, entry_lower) # is a dir, higher priority, so 0
     else:
-        return (1, entry_lower) # not a dir, lower priority
+        return (1, entry_lower) # not a dir, lower priority, so 1
 
 def lsdir():
     output = sorted(os.scandir("."), key=sort_helper)
     for entry in output:
         stat = entry.stat()
         if entry.is_dir():
-            print(f"dir: {entry.name}")
+            print(colored(f"{entry.name}/", "blue"))
         else:
-            print(f"entry: {entry.name}")
+            print(colored(f"{entry.name}", "white"))
 
 def main():
     lsdir()
