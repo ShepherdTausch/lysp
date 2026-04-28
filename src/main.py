fb3f4790bfd2ee81570f413aa8ffcffd266b7cab
ShepherdTausch <238777388+ShepherdTausch@users.noreply.github.com>
2026-04-28 13:40:14 -0400

    Add colors and uniform printing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,53 @@
 import os, sys
+import stat
 import pwd
 from datetime import datetime
 from termcolor import colored
+from humanize import naturalsize
+
+PERM_COLORS = {
+    "d": "blue",
+    "r": "yellow",
+    "w": "red",
+    "x": "green",
+    "-": "light_grey"
+}
+
+class Entry():
+    def __init__(self, entry, info):
+        self.is_dir = entry.is_dir()
+        self.permissions = stat.filemode(info.st_mode)
+        if self.is_dir:
+            self.size = "-"
+        else:
+            self.size = naturalsize(info.st_size, binary=True)
+        self.owner = pwd.getpwuid(info.st_uid).pw_name
+        self.date_modified = datetime.fromtimestamp(info.st_mtime).strftime('%d %b %H:%M')
+        self.name = entry.name
+
+    def add_colors(self):
+        # Permissions
+        colored_letters = [colored(l, PERM_COLORS[l]) for l in self.permissions]
+        letters_str = "".join(colored_letters)
+        self.permissions = letters_str
+        
+        # Size
+        if self.is_dir:
+            self.size = colored("-", "light_grey")
+        else:
+            self.size = colored(self.size, "green")
+
+        # Owner
+        self.owner = colored(self.owner, "yellow")
+
+        # Date Modified
+        self.date_modified = colored(self.date_modified, "blue")
+
+        # Name
+        if self.is_dir:
+            self.name = colored(self.name, "light_blue")
+        else:
+            self.name = colored(self.name, "white")
 
 def sort_helper(entry):
     entry_lower = entry.name.lower()
@@ -12,12 +58,16 @@ def sort_helper(entry):
 
 def lsdir():
     output = sorted(os.scandir("."), key=sort_helper)
-    for entry in output:
-        stat = entry.stat()
-        if entry.is_dir():
-            print(colored(f"{entry.name}/", "blue"))
-        else:
-            print(colored(f"{entry.name}", "white"))
+    for item in output:
+        # Get stat iterable as "info"
+        info = item.stat()
+        # Properties
+        entry = Entry(item, info)
+        # Add color
+        entry.add_colors()
+        
+        output = f"{entry.permissions} {entry.size} {entry.owner} {entry.date_modified} {entry.name}"
+        print(output)
 
 def main():
     lsdir()
